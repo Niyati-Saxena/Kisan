ff674ff8df0535c95d15bc77125dcca51dd747a3
Niyati-Saxena <niyatisaxena31@gmail.com>
2026-08-17 15:36:53 +0530

    Structured Weather Module

--- server/kisan-server/src/main/java/com/kisan/repository/Weather_DataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kisan.repository;
-
-import com.kisan.model.Weather_Data;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Weather_DataRepository extends JpaRepository<Weather_Data, Long> {
-
-    
-}
