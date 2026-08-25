491a613c164ebfa7564696d976b8d458516dc1cd
Niyati-Saxena <niyatisaxena31@gmail.com>
2026-08-26 00:32:16 +0530

    Move registration to dedicated route

--- client/kisan-client/src/App.js
+++ client/kisan-client/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import NavBar from './components/NavBar';
 import HeroCarousel from './components/HeroCarousel';
 import ServicesSection from './features/services/ServicesSection';
 import WhyChooseUs from './components/About/WhyChooseUs';
 import ContactForm from './features/communication/ContactForm';
 import Footer from './components/Footer';
-import Marketplace from './features/marketplace/MarketPlace'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import Marketplace from './features/marketplace/MarketPlace';
 import WeatherDashboard from './features/weather/WeatherDashboard';
 import NewsFeed from './features/news/NewsFeed';
 import KnowledgePage from './features/knowledge/KnowledgePage';
@@ -15,12 +15,22 @@ import About from './pages/About';
 import CropList from './features/crop/CropList';
 import Register from './components/Register';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 function App() {
   return (
     <Router>
+
       <NavBar />
+
       <main>
+
         <Routes>
+
+          {/* =========================
+              PUBLIC HOME PAGE
+          ========================= */}
           <Route
             path="/"
             element={
@@ -28,20 +38,64 @@ function App() {
                 <HeroCarousel />
                 <ServicesSection />
                 <WhyChooseUs />
-                <Register/>
               </>
             }
           />
-          <Route path="/marketplace" element={<Marketplace />} />
-          <Route path="/weather" element={<WeatherDashboard/>}/> 
-          <Route path="/news" element={<NewsFeed/>}/>
-          <Route path="/knowledge" element = {<KnowledgePage/>}/>
-          <Route path="/contact" element= {<ContactForm/>}/>
-          <Route path = "/about" element={<About/>}/>
-          <Route path = "/crops" element = {<CropList/>}/>
-       </Routes>
+
+          {/* =========================
+              PUBLIC PAGES
+          ========================= */}
+
+          <Route
+            path="/marketplace"
+            element={<Marketplace />}
+          />
+
+          <Route
+            path="/weather"
+            element={<WeatherDashboard />}
+          />
+
+          <Route
+            path="/news"
+            element={<NewsFeed />}
+          />
+
+          <Route
+            path="/knowledge"
+            element={<KnowledgePage />}
+          />
+
+          <Route
+            path="/contact"
+            element={<ContactForm />}
+          />
+
+          <Route
+            path="/about"
+            element={<About />}
+          />
+
+          <Route
+            path="/crops"
+            element={<CropList />}
+          />
+
+          {/* =========================
+              AUTHENTICATION
+          ========================= */}
+
+          <Route
+            path="/register"
+            element={<Register />}
+          />
+
+        </Routes>
+
       </main>
+
       <Footer />
+
     </Router>
   );
 }
